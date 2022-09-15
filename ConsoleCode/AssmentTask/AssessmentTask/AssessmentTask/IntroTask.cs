52a8a4bf0ea65f0d392495f08a5f031b4d64f264
Jonathan <jarzate26.ja10@gmail.com>
2022-09-15 09:24:24 -0700

    added array and fixed intro task

--- ConsoleCode/AssmentTask/AssessmentTask/AssessmentTask/IntroTask.cs
+++ ConsoleCode/AssmentTask/AssessmentTask/AssessmentTask/IntroTask.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 
-/*
- *  Brandon Eiler 8 / 26 / 2022 Intro to C# Assessment
- *  Task 1: Introductory Exercises
- */
+
 namespace AIE
 {
     class IntroTask
@@ -11,39 +8,24 @@ namespace AIE
         static void Main()
         {
             Console.WriteLine(AddNumbers(1.1f, 3.89f));
-            //should output 4 even though the float sum is 4.99f
+            
             Console.WriteLine(AddNumbers(5.1f, 3.89f));
-            //should output 8 even though the float sum is 8.99f
+            
             Console.WriteLine(AddNumbers(0.5f, 2.89f));
-            //should output 3 even though the float sum is 3.39f
+           
 
             Fibonacci();
 
             FizzBuzz();
 
         }
-        //Exercise 1: Adding Two Numbers
-        /*  
-            Determine the integer floor of the sum of two floating point numbers.
-            The floor function is the function that takes as input a real number x, and gives as output the greatest integer less than or equal to x.
-            Create a function called AddNumbers that accepts two parameters of type float, and returns an integer.
-            Example:
-            floor(1.1 + 3.89) = floor(4.99) = 4 
-        */
+        
         static int AddNumbers(float x, float y)
         {
-            //cast the sum of x and y to type int
             return (int)(x + y);
         }
 
-        //Exercise 2: Fibonacci
-        /*
-            The Fibonacci numbers are a sequence of numbers where each number after the first two is a sum of the prior two. 
-            As an illustration, here is a short sequence given starting values of (0, 1):
-            Fibonacci series =  (0, 1, 1, 2, 3, 5, 8, 13)
-            Given an integer n, calculate the first n numbers in the Fibonacci sequence given starting elements of (0, 1). 
-            Print out n integers, including the given (0, 1) in the sequence.
-         */
+      
         static void Fibonacci()
         {
             string output = "";
@@ -71,15 +53,7 @@ namespace AIE
             Console.WriteLine(output);
         }
 
-        //Exercise 3: FizzBuzz
-        /* 
-            Given a number n, for each integer i in the range from 1 to n inclusive, print one value per line as follows:
-                >If i is a multiple of both 3 and 5, print FizzBuzz.
-                >If i is a multiple of 3 (but not 5), print Fizz.
-                >If i is a multiple of 5 (but not 3), print Buzz.
-                >If i is not a multiple of 3 or 5, print the value of i.
-            The function must print the appropriate response for each value i in the set {1, 2, ... n} in ascending order, each on a separate line.
-         */
+       
         static void FizzBuzz()
         {
             int startingNumber = 1;
